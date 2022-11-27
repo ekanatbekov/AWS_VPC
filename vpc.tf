b45a1aebbe8a742365ba653746cf0430c1c882d1
ekanatbekov <kanatbekovesen@gmail.com>
2022-11-27 15:08:31 -0600

    the first configuration of the VPC

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.available_zones
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
+
+  tags = var.tags
+}
